b861ec5dc84370020cc102d4e8d335525e408179
kramsey458 <kramsey458@gmail.com>
2019-02-26 16:36:06 -0800

    JDBC url fix

--- src/main/java/JDBC/Constants.java
+++ src/main/java/JDBC/Constants.java
@@ -3,5 +3,5 @@ package JDBC;
 public class Constants {
     static final String username = "meets";
     static final String password = "123abc";
-    static final String dbUrl = "jdbc:mysql://ambari-head.csc.calpoly:3306/";
+    static final String dbUrl = "jdbc:mysql://ambari-head.csc.calpoly.edu:3306/";
 }
